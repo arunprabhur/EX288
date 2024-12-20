e101ee587c72e64c5dcc03647d4039d22a17b6d2
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-20 06:18:40 -0500

    updated

--- lab2/lab2-execute.sh
+++ lab2/lab2-execute.sh
@@ -2,15 +2,13 @@
 
 source ../common.sh
 
-echo -e  "\n Starting script --> Creating an app using docker file and run it in Openshift Cluster"
+echo -e  "\n Starting script --> Creating an app using docker file from git and run it in Openshift Cluster"
 
 prompt
 
-execute "Creating an image for bash script" '''podman build -t registry.ocp4.example.com:8443/developer/mybashapp:1.0 .'''
-execute "Pushing image into temp registry" '''podman push registry.ocp4.example.com:8443/developer/mybashapp:1.0''' 
 execute "login to OCP4" '''oc login -u developer -p developer https://api.ocp4.example.com:6443'''
 execute "Create New Project" '''oc new-project mylab1'''
-execute "Creating New app for the bash app" '''oc new-app --image registry.ocp4.example.com:8443/developer/mybashapp:1.0 --name mybashapp'''
+execute "Creating New app for the bash app" '''oc new-app --strategy docker --name mybashapp --context-dir /lab2 https://github.com/arunprabhur/EX288.git'''
 execute "check pods running status" '''oc get po'''
 clear
 execute "View all resources created" ''' oc get all'''
