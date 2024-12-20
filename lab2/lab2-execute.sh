53546ca331f4be957880f5eeff194b79da25628d
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-20 07:46:09 -0500

    added more info

--- lab2/lab2-execute.sh
+++ lab2/lab2-execute.sh
@@ -8,8 +8,14 @@ prompt
 
 execute "login to OCP4" '''oc login -u developer -p developer https://api.ocp4.example.com:6443'''
 execute "Create New Project" '''oc new-project mylab1'''
-execute "Creating New app for the bash app" '''oc new-app --strategy docker --name mybashapp --context-dir /lab2 https://github.com/arunprabhur/EX288.git'''
+clear
+echo -e "To run a local folder \n 1. oc new-app --name mybashapp --strategy docker \n"
+echo -e "2. oc start-build buildconfig/mybashapp --from-dir ."
+read -p "Enter to continue"
+execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab2 https://github.com/arunprabhur/EX288.git'''
+sleep 15
 execute "check pods running status" '''oc get po'''
+sleep 15
 clear
 execute "View all resources created" ''' oc get all'''
 execute "Scale up to 5 pods" '''oc scale --replicas 5 deployment/mybashapp'''
