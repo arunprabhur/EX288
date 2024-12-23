05a99866039e7910cf9e2eb3e0b8664ddb3067a0
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-23 07:48:15 -0500

    added image stream

--- lab3/lab3-execute.sh
+++ lab3/lab3-execute.sh
@@ -14,6 +14,9 @@ execute "check secrets" '''oc get secret'''
 execute "describe generic secret" '''oc get secret example-secret -o yaml'''
 execute "create docker registry secret" '''oc create secret docker-registry quay-secret --docker-server registry.ocp4.example.com:8443 --docker-username developer --docker-password developer --docker-email prabhu.arun10@gmail.com'''
 execute "describe docker secrets quay-secret" '''oc get secret quay-secret -o yaml'''
+execute "Importing Images from external registry to image stream" '''oc import-image ubi8/openjdk-21:1.21-1.1733300817 --from=registry.access.redhat.com/ubi8/openjdk-21:1.21-1.1733300817 --confirm'''
+execute "Check Image stream in project" '''oc get is'''
+execute "Describe tags of the image stream" '''oc describe is openjdk'''
 execute "Deleting Project" '''oc delete project mylab1'''
 endofscript
 
