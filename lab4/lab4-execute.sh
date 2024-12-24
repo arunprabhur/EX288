1da2fd86ec4a411c1dd2b97722386dd18577a591
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-24 09:08:06 -0500

    aadded images streams

--- lab4/lab4-execute.sh
+++ lab4/lab4-execute.sh
@@ -12,9 +12,11 @@ execute "Create only build - New Build" ''' oc new-build --name mybashapp --cont
 
 execute "view the status of build, Ctrl+C to terminate" '''oc get po -w'''
 execute "view build config Name" '''oc get bc'''
+execute "addign more logger to build" '''oc set env bc/mybashapp BUILD_LOGLEVEL="3"'''
 execute "Describe build config for mybashapp yaml" '''oc describe bc/mybashapp'''
+execute "Get BuildConfig Yaml fopr mybashapp" '''oc get bc/mybashpp -o yaml'''
 execute "Start a new Build" '''oc start-build bc/mybashapp'''
-
+execute "View detailed logs for build 2" '''oc logs pod/mybashapp-2-build'''
 execute "view the status of build, Ctrl+C to terminate" '''oc get po -w'''
 execute "Deleting Project" '''oc delete project mylab1'''
 endofscript
