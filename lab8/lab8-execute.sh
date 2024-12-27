6b46966f2919b10f8dbabe190ba497f439215ec6
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-27 05:49:17 -0500

    remove app ayaml

--- lab8/lab8-execute.sh
+++ lab8/lab8-execute.sh
@@ -9,7 +9,8 @@ prompt
 execute "login to OCP4" '''oc login -u developer -p developer https://api.ocp4.example.com:6443'''
 execute "Create New Project" '''oc new-project mylab1'''
 execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab8 https://github.com/arunprabhur/EX288.git --as-deployment-config -o yaml > application.yaml'''
-execute "Check whether new build is created and running" '''oc get po -w'''
+execute "Apply the app configfile" '''oc apply -f application.yaml'''
+execute "Check whether new build is created and running" '''watch -n 2 oc get po'''
 execute "Verify events" '''oc get events --sort-by .lastTimestamp'''
 clear
 echo -e "You can debug this issue by comparing the docker file and analzying the failed pod \n hint - oc debug command -- try in new terminaal session"
