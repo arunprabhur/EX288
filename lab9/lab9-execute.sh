4ff838b653c997e3c07be761cda61e9c91781558
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-29 12:40:01 -0500

    added new lab

--- lab9/lab9-execute.sh
+++ lab9/lab9-execute.sh
@@ -8,10 +8,13 @@ prompt
 
 execute "login to OCP4" '''oc login -u developer -p developer https://api.ocp4.example.com:6443'''
 execute "Create New Project" '''oc new-project mylab1'''
-execute "create a secret from literal" '''oc create secret generic mybashappsecret --from-literal BASHAPPENV=SECRETVALUE'''
-execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab9 -e BASHAPP=secret/mybashappsecret  https://github.com/arunprabhur/EX288.git '''
-
+execute "create a secret from literal" '''oc create secret generic mybashappsecret --from-literal BASH_SECRET_ENV=ENV_SECRETVALUE'''
+execute "create a config map from literal" '''oc create cm mybashcm --from-literal BASH_CM_SECRET=CM_SECRET'''
+execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab9  https://github.com/arunprabhur/EX288.git '''
+execute "Setting the secret to environment variable of the pods" '''oc set env deployment mybashapp --from secret/mybashappsecret'''
+execute "Setting the cm to environment variable of the pods" '''oc set env deployment mybashapp --from cm/mybashcm'''
 execute "Check whether new build is created and running" '''watch -n 2 oc get po'''
+execute "Deleting the project" '''oc delete project mylab1'''
 endofscript
 
 echo "End of script"
