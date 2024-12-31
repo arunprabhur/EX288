558ee880839c7039d629a7728c3e83c911bd1e6b
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-31 01:53:36 -0500

    addedifx

--- lab11/lab11-execute.sh
+++ lab11/lab11-execute.sh
@@ -8,7 +8,7 @@ prompt
 
 execute "login to OCP4" '''oc login -u developer -p developer https://api.ocp4.example.com:6443'''
 execute "Create New Project" '''oc new-project mylab1'''
-execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab1111111111111111111111  https://github.com/arunprabhur/EX288.git '''
+execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab11  https://github.com/arunprabhur/EX288.git '''
 execute "setting persistent volume for the application" '''oc set volumes deployment/mybashapp --add -t pvc --claim-mode rwo --claim-name test-pvc-claim --claim-size 2Gi --mount-path /shared --name shared-volume'''
 execute "Check whether new pvc is created" '''watch -n 2 oc get pvc'''
 
