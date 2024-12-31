f80b5c060d45fc0f75de05c55c1b98cf3a677fd1
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-31 02:58:19 -0500

    completed lab11

--- lab11/lab11-execute.sh
+++ lab11/lab11-execute.sh
@@ -2,7 +2,7 @@
 
 source ../common.sh
 
-echo -e  "\n Starting script --> create secrets and config map,apply it into a pod as env and mount as a file in Openshift Cluster"
+echo -e  "\n Starting script --> create pvc and apply into bash app, create app with statefulset with different pvc in Openshift Cluster"
 
 prompt
 
@@ -10,8 +10,38 @@ execute "login to OCP4" '''oc login -u developer -p developer https://api.ocp4.e
 execute "Create New Project" '''oc new-project mylab1'''
 execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab11  https://github.com/arunprabhur/EX288.git '''
 execute "setting persistent volume for the application" '''oc set volumes deployment/mybashapp --add -t pvc --claim-mode rwo --claim-name test-pvc-claim --claim-size 2Gi --mount-path /shared --name shared-volume'''
-execute "Check whether new pvc is created" '''watch -n 2 oc get pvc'''
+execute "Check whether new pvc is created" '''watch -n 2 oc get pvc,pod'''
 
+
+echo -e  "Copying a text file into a pod and store int the shared drive\n oc cp sample-shared-file.txt $(oc get po | grep Running | cut -d " " -f1):/shared/ "
+oc cp sample-shared-file.txt $(oc get po | grep Running | cut -d " " -f1):/shared/
+read -p "press enter to continue"
+clear
+echo -e "Print the shared file data from pod \n oc rsh $(oc get po | grep Running | cut -d " " -f1) cat /shared/sample-shared-file.txt"
+oc rsh $(oc get po | grep Running | cut -d " " -f1) cat /shared/sample-shared-file.txt
+read -p "press enter to continue"
+clear
+
+echo -e "delete existing running pod oc delete pod \n $(oc get po | grep Running | cut -d " " -f1)"
+oc delete pod $(oc get po | grep Running | cut -d " " -f1)
+read -p "Enter to continue" 
+clear
+sleep 10
+
+echo -e "Print the shared file data from new pod \n oc rsh $(oc get po | grep Running | cut -d " " -f1) cat /shared/sample-shared-file.txt"
+oc rsh $(oc get po | grep Running | cut -d " " -f1) cat /shared/sample-shared-file.txt
+read -p "press enter to continue"
+clear
+
+execute "delete deployment, so we can try statefulset" '''oc delete deployment mybashapp'''
+execute "Apply statefulset" '''oc apply -f mybashapp-stateful-set.yaml'''
+
+execute "watch statefulset, po, pvc" '''watch oc get po,pvc,statefulset'''
+
+execute "scale to 5" '''oc scale statefulset mybashapp --replicas 5'''
+
+
+execute "watch statefulset, po, pvc" '''watch oc get po,pvc,statefulset'''
 execute "Deleting the project" '''oc delete project mylab1'''
 endofscript
 
