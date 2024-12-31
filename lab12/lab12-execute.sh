775e8e5f211cb69df34eb0fa7e942139f1b3ff57
Arun Prabhu <prabhu.arun10@gmail.com>
2024-12-31 06:19:44 -0500

    fixed issues

--- lab12/lab12-execute.sh
+++ lab12/lab12-execute.sh
@@ -8,7 +8,7 @@ prompt
 
 execute "login to OCP4" '''oc login -u developer -p developer https://api.ocp4.example.com:6443'''
 execute "Create New Project" '''oc new-project mylab1'''
-execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab122 https://github.com/arunprabhur/EX288.git '''
+execute "Creating New app for the bash app from git" '''oc new-app --strategy docker --name mybashapp --context-dir /lab12 https://github.com/arunprabhur/EX288.git '''
 execute "watch  pods" '''watch oc get po'''
 execute "Deleting the project" '''oc delete project mylab1'''
 endofscript
